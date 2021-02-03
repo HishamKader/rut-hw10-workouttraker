79e44f3cd06eabcb56694bb8417742ecc69503cf
Hisham Kader <hisham319@hotmail.com>
2021-02-02 20:02:43 -0500

    debbuged database issue

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+var path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const db = require("./models");
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {useNewUrlParser: true});
 
-// routes
-app.use(require("./routes/api.js"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", { useNewUrlParser: true });
 
-app.use(require("./routes/htmlRoutes.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+
+
+// Bring in our routes
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("App running on port 3000!");
+  });
+  
