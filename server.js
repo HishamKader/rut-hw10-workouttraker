d8a107012ff7b6662b7e8d904d6d2451f4087c9b
Hisham Kader <hisham319@hotmail.com>
2021-02-02 19:54:38 -0500

    added debbuging for server

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {useNewUrlParser: true});
 
 // routes
 app.use(require("./routes/api.js"));
